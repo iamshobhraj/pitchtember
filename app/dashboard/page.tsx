f27d4a4fedf640e47c6dfb8aa901d40c49bc441b
iamshobh <iamshobhraj@gmail.com>
2023-11-19 05:28:21 +0530

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import readUserSession from "@/lib/actions";
+import {redirect} from "next/navigation";
+import Profile from "@/app/dashboard/components/profile";
+import SignOut from "@/app/dashboard/components/SignOut";
+
+export default async function Page() {
+
+    const {data} = await readUserSession();
+
+    // if (!data.session){
+    //     return redirect("/auth-server-action");
+    // }
+
+    return (
+        <main className={'p-36 bg-white h-screen'}>
+            <SignOut />
+            <div className={'rounded-2xl h-full'}>
+                <Profile session={data.session}></Profile>
+            </div>
+        </main>
+    )
+
+
+
+
+}

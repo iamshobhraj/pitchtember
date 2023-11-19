6d455536c2e9b2cd02a5e10a583760eb7a70ef93
iamshobh <iamshobhraj@gmail.com>
2023-11-19 13:27:33 +0530

    routing fix

--- app/auth-server-action/callback/route.ts
+++ app/auth-server-action/callback/route.ts
@@ -31,7 +31,7 @@ export async function GET(request: Request) {
         )
         const { error } = await supabase.auth.exchangeCodeForSession(code)
         if (!error) {
-            return NextResponse.redirect("https://pitchtember.vercel.app/dashboard")
+            return NextResponse.redirect(requestUrl.origin + "/dashboard")
         }
     }
 

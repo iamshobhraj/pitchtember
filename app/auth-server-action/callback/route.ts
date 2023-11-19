ae4cd6f489308f6e64d217c7538a7756dd91906d
iamshobh <iamshobhraj@gmail.com>
2023-11-19 12:05:42 +0530

    fixed linting issue

--- app/auth-server-action/callback/route.ts
+++ app/auth-server-action/callback/route.ts
@@ -5,7 +5,7 @@ import { type CookieOptions, createServerClient } from '@supabase/ssr'
 export async function GET(request: Request) {
     const requestUrl = new URL(request.url)
 
-    const { searchParams, origin } = requestUrl;
+    const { searchParams} = requestUrl;
     const code = searchParams.get('code')
     // if "next" is in param, use it as the redirect URL
     const next = searchParams.get('next') ?? '/'
